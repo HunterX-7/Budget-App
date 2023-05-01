7e4885944a3609d619e296577e178f61541854bf
HunterX-7 <fraquin7@gmail.com>
2023-05-01 17:13:01 -0500

    Add controller before_action login methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+          user_params.permit(:email, :encrypted_password)
+        end
+    
+        devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+          user_params.permit(:name, :email, :password, :password_confirmation)
+        end
+    end
 end
